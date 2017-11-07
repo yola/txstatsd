c62f9864be6bdff8fd49ced06e6626ec0e50af5e
fr33l <andrii.zakharkiv@synthasite.com>
2017-11-07 17:11:39 +0200

    Use DaemonApp on yodeploy 0.8.4

--- deploy/hooks.py
+++ deploy/hooks.py
@@ -3,19 +3,19 @@ import logging
 from yodeploy.hooks.configurator import ConfiguratedApp
 from yodeploy.hooks.python import PythonApp
 from yodeploy.hooks.templating import TemplatedApp
-from yodeploy.hooks.upstart import UpstartApp
+from yodeploy.hooks.daemon import DaemonApp
 from yodeploy.util import touch
 
 log = logging.getLogger(__name__)
 
 
-class Hooks(ConfiguratedApp, PythonApp, UpstartApp, TemplatedApp):
+class Hooks(ConfiguratedApp, PythonApp, DaemonApp, TemplatedApp):
     def prepare(self):
         super(Hooks, self).prepare()
         self.template('txstatsd.conf.template', '/etc/yola/txstatsd.conf')
 
         logfile = self.config.get(self.app).path.log
         if logfile:
-            touch(logfile, 'root', 'adm', 0640)
+            touch(logfile, 'root', 'adm', 0o640)
 
 hooks = Hooks
